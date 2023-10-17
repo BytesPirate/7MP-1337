be5708aec5a94e793c14fc2286e972dc1675691b
J1nH4ng <jinhang@chinacici.com>
2023-10-17 09:10:41 +0800

    feat(daily):2023-10-17 的 leetcode 每日一题

---
+++ daily/2023_10_17_2652_sum_of_multiples/src/main.rs
@@ -0,0 +1,21 @@
+/*
+    Author: J1n H4ng
+    Date: 2023/10/17
+    Program: 2652. Sum of Multiples
+    Link: https://leetcode.cn/problems/sum-multiples/description/
+    Description: 1 到 n 中，3，5，7的整数倍之和
+ */
+struct Solution;
+
+impl Solution {
+    pub fn sum_of_multiples(n: i32) -> i32 {
+        return 0;
+    }
+}
+
+fn main() {
+    let n = 7;
+
+    let result = Solution::sum_of_multiples(n);
+    println!("{}", result);
+}
