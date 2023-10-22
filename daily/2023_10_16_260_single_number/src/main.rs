e45b933b1384585649f0fcbf314dba06460a81d1
J1nH4ng <jinhang@mail.14bytes.com>
2023-10-22 23:29:02 +0800

    feat(*):leetcode 题目

--- daily/2023_10_16_260_single_number/src/main.rs
+++ daily/2023_10_16_260_single_number/src/main.rs
@@ -1,3 +1,37 @@
+struct Solution;
+
+impl Solution {
+    pub fn single_number(nums: Vec<i32>) -> Vec<i32> {
+        let n = nums.len();
+        let mut ans :i32 = 0;
+
+        for i in 0..n{
+            ans ^= nums[i];
+        }
+
+        let low_bit :i32 = ans & (-ans);
+
+        let mut ans_1 :i32 = 0;
+        let mut ans_2 :i32 = 0;
+
+        for i in 0..n {
+            if low_bit & nums[i] == 0 {
+                ans_1 ^= nums[i];
+            }else{
+                ans_2 ^= nums[i];
+            }
+        }
+
+        let mut ans_vec = vec![0;2];
+        ans_vec[0] = ans_1;
+        ans_vec[1] = ans_2;
+        ans_vec
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let nums = [1, 2, 1, 3, 2, 5].to_vec();
+
+    let result = Solution::single_number(nums);
+    println!("{:?}", result);
 }
---
+++ daily/2023_10_21_2316_count_pairs/src/main.rs
@@ -0,0 +1,14 @@
+struct Solution;
+
+impl Solution {
+    pub fn count_pairs(n: i32, edges: Vec<Vec<i32>>) -> i64 {
+
+    }
+}
+
+fn main() {
+    let n = 3;
+    let edges = [[0, 1], [0, 2], [1, 2]];
+
+
+}
