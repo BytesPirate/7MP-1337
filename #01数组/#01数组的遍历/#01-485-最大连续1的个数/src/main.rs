86dee092e7a216cb7f061f13069a0f6e78c3d089
J1nH4ng <jinhang@chinacici.com>
2024-01-08 13:35:45 +0800

    style(优化目录解构):
    Signed-off-by: J1nH4ng <jinhang@chinacici.com>

---
+++ #01数组/#01数组的遍历/#01-485-最大连续1的个数/src/main.rs
@@ -0,0 +1,14 @@
+struct  Solution;
+
+impl Solution {
+    pub fn find_max_consecutive_ones(nums: Vec<i32>) -> i32 {
+        let mut ans = 0;
+
+        return ans;
+    }
+}
+fn main() {
+    let nums = vec![1,1,0,1,1,1];
+
+    println!("{}", Solution::find_max_consecutive_ones(nums));
+}
