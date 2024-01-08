3091b8cb3db672a43bd669beac4ba4da751c03b4
J1nH4ng <jinhang@chinacici.com>
2024-01-08 14:41:03 +0800

    feat(#01-#01-#01): 完成数组第一部分的第一题，最大的连续1的个数
    Signed-off-by: J1nH4ng <jinhang@chinacici.com>

--- #01数组/#01数组的遍历/#01-485-最大连续1的个数/src/main.rs
+++ #01数组/#01数组的遍历/#01-485-最大连续1的个数/src/main.rs
@@ -4,6 +4,21 @@ impl Solution {
     pub fn find_max_consecutive_ones(nums: Vec<i32>) -> i32 {
         let mut ans = 0;
 
+        let mut count = 0;
+
+        for i in 0..nums.len() {
+            if nums[i] == 1 {
+                count = count + 1;
+                if ans > count {
+                    ans = ans;
+                }else{
+                    ans = count;
+                }
+            }else{
+                count = 0
+            }
+        }
+
         return ans;
     }
 }
